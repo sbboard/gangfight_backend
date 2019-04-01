bdc9f93174036908579f4fcfd2d633ba4d682f40
sbboard <colin.buffum@gmail.com>
2019-04-01 00:32:08 -0600

    installed cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const dbpass = require('./secret.js')
@@ -11,7 +12,18 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const app = express()
-
+let whitelist = [
+    'http://107.188.145.8:8080',
+    'http://159.65.188.38:80',
+];
+let corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
