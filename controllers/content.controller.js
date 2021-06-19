2095caf9a10a443ab2911c84752515abc9fc870f
sbboard <colin.buffum@gmail.com>
2021-06-19 15:22:58 -0600

    single page comics can be uploaded / URL scrubbed better

--- controllers/content.controller.js
+++ controllers/content.controller.js
@@ -1,135 +1,153 @@
-const Product = require('../models/content.model')
-var fs = require('fs');
+const Product = require("../models/content.model");
+var fs = require("fs");
 
-function searchIdPromise(id){
-    var promise = Product.find({_id:id}).exec();
-    return promise;
- }
+function searchIdPromise(id) {
+  var promise = Product.find({ _id: id }).exec();
+  return promise;
+}
 
 exports.product_create = (req, res, next) => {
-    //handle thumbnail image
-    let name = req.files.img.name
-    let detUrl = ""
-    let detArray = []
-    req.files.img.mv('/var/www/html/assets/contentImages/'+name)
-    //handle comicsArray or URL
-    if(req.body.comicSource == "Upload"){
-        let projNameNoSpecial = req.body.title.replace(/\s/g, '') + req.body.subtitle.replace(/\s/g, '')
-        let dir = `/var/www/html/assets/comics/${projNameNoSpecial}`
-        detUrl = projNameNoSpecial
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
-        for(let i=0;i<req.files.pages.length;i++){
-            req.files.pages[i].mv(dir+"/"+req.files.pages[i].name)
-            detArray.push(req.files.pages[i].name)
-        }
+  //handle thumbnail image
+  let name = req.files.img.name;
+  let detUrl = "";
+  let detArray = [];
+  req.files.img.mv("/var/www/html/assets/contentImages/" + name);
+  //handle comicsArray or URL
+  if (req.body.comicSource == "Upload") {
+    let projNameNoSpecial =
+      req.body.title.replace(/[^\w]/gi, "") +
+      req.body.subtitle.replace(/[^\w]/gi, "");
+    let dir = `/var/www/html/assets/comics/${projNameNoSpecial}`;
+    detUrl = projNameNoSpecial;
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
-    else{
-        detUrl = req.body.url
+    if (Array.isArray(req.files.pages)) {
+      for (let i = 0; i < req.files.pages.length; i++) {
+        req.files.pages[i].mv(dir + "/" + req.files.pages[i].name);
+        detArray.push(req.files.pages[i].name);
+      }
+    } else {
+      req.files.pages.mv(dir + "/" + req.files.pages.name);
+      detArray.push(req.files.pages.name);
     }
-    let product = new Product({
-        title: req.body.title,
-        subtitle: req.body.subtitle,
-        img: name,
-        url: detUrl,
-        category: req.body.category,
-        date: Date(),
-        series: req.body.series,
-        comicsArray: detArray,
-        updatedDate: Date(),
-    })
+  } else {
+    detUrl = req.body.url;
+  }
+  let product = new Product({
+    title: req.body.title,
+    subtitle: req.body.subtitle,
+    img: name,
+    url: detUrl,
+    category: req.body.category,
+    date: Date(),
+    series: req.body.series,
+    comicsArray: detArray,
+    updatedDate: Date(),
+  });
 
-    product.save((err) => {
-        if (err){
-            return next(err)
-        }
-        res.send('Content Posted')
-    })
-}
+  product.save((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.send("Content Posted");
+  });
+};
 
 exports.post_update = (req, res, next) => {
-    //get info from old post
-    let promise = searchIdPromise(req.body.contentUpdating)
-    promise.then((oldContent)=>{
-        //handle thumbnail
-        console.log(oldContent)
-        if(req.hasOwnProperty('files')){
-            name = req.files.img.name
-            req.files.img.mv('/var/www/html/assets/contentImages/'+name)
-        }
-        else{
-            name = oldContent.img
-        }
-        //add new update for homepage
-        let product = new Product({
-            title: oldContent[0].title,
-            subtitle: req.body.subtitle,
-            img: name,
-            url: oldContent[0].url,
-            category: "update",
-            date: Date(),
-            series: oldContent[0].series,
-            comicsArray: oldContent[0].comicsArray,
-            updatedDate: Date(),
-        })
-        return {'product': product, 'name': name, 'updating': req.body.contentUpdating}
+  //get info from old post
+  let promise = searchIdPromise(req.body.contentUpdating);
+  promise
+    .then((oldContent) => {
+      //handle thumbnail
+      console.log(oldContent);
+      if (req.hasOwnProperty("files")) {
+        name = req.files.img.name;
+        req.files.img.mv("/var/www/html/assets/contentImages/" + name);
+      } else {
+        name = oldContent.img;
+      }
+      //add new update for homepage
+      let product = new Product({
+        title: oldContent[0].title,
+        subtitle: req.body.subtitle,
+        img: name,
+        url: oldContent[0].url,
+        category: "update",
+        date: Date(),
+        series: oldContent[0].series,
+        comicsArray: oldContent[0].comicsArray,
+        updatedDate: Date(),
+      });
+      return {
+        product: product,
+        name: name,
+        updating: req.body.contentUpdating,
+      };
     })
-    .then((deliverable)=>{
-        //update old project
-        Product
-        .findByIdAndUpdate(deliverable.updating, { $set: {
-            updatedDate: Date(),
-            img: deliverable.name,
-        }}).exec()
-        return deliverable.product
+    .then((deliverable) => {
+      //update old project
+      Product.findByIdAndUpdate(deliverable.updating, {
+        $set: {
+          updatedDate: Date(),
+          img: deliverable.name,
+        },
+      }).exec();
+      return deliverable.product;
     })
-    .then((product)=>
-    product.save((err) => {
-        if (err){
-            return next(err)
+    .then((product) =>
+      product.save((err) => {
+        if (err) {
+          return next(err);
         }
-        res.send('Update Posted')
-    }))
-}
+        res.send("Update Posted");
+      })
+    );
+};
 
 exports.whole_list = (req, res, next) => {
-    Product.find({}).sort('-date').exec((err,personList) => {
-        if (err) return next(err)
-        if(isNaN(req.params.num)){
-            res.send(personList)
-        }
-        else{
-            res.send(personList.slice(0,req.params.num))
-        }
-    })
-}
+  Product.find({})
+    .sort("-date")
+    .exec((err, personList) => {
+      if (err) return next(err);
+      if (isNaN(req.params.num)) {
+        res.send(personList);
+      } else {
+        res.send(personList.slice(0, req.params.num));
+      }
+    });
+};
 
 exports.category_list = (req, res, next) => {
-    Product.find({category:req.params.cat}).sort('-updatedDate').exec((err,personList) => {
-        if (err) return next(err)
-        res.send(personList)
-    })
-}
+  Product.find({ category: req.params.cat })
+    .sort("-updatedDate")
+    .exec((err, personList) => {
+      if (err) return next(err);
+      res.send(personList);
+    });
+};
 
 exports.comic_info = (req, res, next) => {
-    Product.find({_id:req.params.id}).exec((err,personList) => {
-        if (err) return next(err)
-        res.send(personList)
-    })
-}
+  Product.find({ _id: req.params.id }).exec((err, personList) => {
+    if (err) return next(err);
+    res.send(personList);
+  });
+};
 
 exports.product_update = (req, res, next) => {
-    Product.findByIdAndUpdate(req.params.id, { $set: req.body},
-        (err, product) => {
-        if (err) return next(err)
-        res.send("Content updated")
-    })
-}
+  Product.findByIdAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    (err, product) => {
+      if (err) return next(err);
+      res.send("Content updated");
+    }
+  );
+};
 
 exports.product_delete = (req, res, next) => {
-    Product.findByIdAndRemove(req.params.id, (err) => {
-        if (err) return next(err)
-        res.send('Deleted successfully')
-    })
-}
+  Product.findByIdAndRemove(req.params.id, (err) => {
+    if (err) return next(err);
+    res.send("Deleted successfully");
+  });
+};
