33b5c8397d19dc761a57738fa695ad5b699e7b90
sbboard <colin.buffum@gmail.com>
2024-08-12 21:40:17 -0400

    add iframe to schema

--- models/content.model.js
+++ models/content.model.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
+const Schema = mongoose.Schema;
 
 const GangContent = new Schema({
-    title: {type: String, required: true},
-    subtitle: {type: String},
-    img: {type: String, required: true},
-    url: {type: String, required: true},
-    category: {type: String, required: true},
-    date: {type: Date, required: true},
-    updatedDate: {type: Date, required: true},
-    series: {type: String},
-    assetFolder: {type: String},
-    comicsArray: {type: Array},
-})
+  title: { type: String, required: true },
+  subtitle: { type: String },
+  img: { type: String, required: true },
+  url: { type: String, required: true },
+  category: { type: String, required: true },
+  date: { type: Date, required: true },
+  updatedDate: { type: Date, required: true },
+  series: { type: String },
+  assetFolder: { type: String },
+  comicsArray: { type: Array },
+  iframe: { type: Boolean, default: false },
+});
 
-module.exports = mongoose.model('content', GangContent)
+module.exports = mongoose.model("content", GangContent);
